db779b9736871b31639142eeb6721f6f83b185cc
MOUSTAFA EZZAT <moustafaelzan6@gmail.com>
2025-03-08 15:49:11 +0200

    Create.Departmeng.Controller

--- Company.Moustafa.PL/Program.cs
+++ Company.Moustafa.PL/Program.cs
@@ -1,3 +1,8 @@
+using Company.Moustafa.BLL.Interfaces;
+using Company.Moustafa.BLL.Repositories;
+using Company.Moustafa.DAL.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Moustafa.PL
 {
     public class Program
@@ -8,6 +13,14 @@ namespace Company.Moustafa.PL
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IDepartmentRepository , DepartmentRepository>();
+            builder.Services.AddDbContext<CompanyDbContext>
+                (options=> {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                
+                });
+
+
 
             var app = builder.Build();
 
